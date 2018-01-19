ad898d148fb1747a8fb15022aa6e185275dcb688
Julia Niall <jnmareike@gmail.com>
2018-01-19 15:20:48 +1300

    sick feedback looping route

--- data.js
+++ data.js
@@ -2,8 +2,14 @@
 // const level2sad = [{id: 'gutted'}, {id: 'mellow'}, {id: 'bummer'}, {id: 'stink'}]
 
 const levelTwo = {
-  happy: [{id: 'stoked'}, {id: 'frothing'}, {id: 'joyful'}, {id: 'peaceful'}],
-  sad: [{id: 'gutted'}, {id: 'mellow'}, {id: 'bummed'}, {id: 'stink'}]
+  happy: [{id: 'stoked', fed: 'Glad to hear you are stoked! Share your happiness with the others and share the love!'},
+    {id: 'frothing', fed: 'That\'s awesome! Keep smiling and enjoy the ride!'},
+    {id: 'joyful', fed: 'Keep playing - life is about having fun!'},
+    {id: 'peaceful', fed: 'Your calmness is sure to spread to others. Remember to keep breathing.'}],
+  sad: [{id: 'gutted', fed: 'Had a bad day? That\'s ok, surround yourself with loved ones and focus on your breath.'},
+    {id: 'mellow', fed: 'We all have those days. Take it easy, choose your favourite activity to relax.'},
+    {id: 'bummed', fed: 'Bugger! Time to take some time off and walk away to focus and refresh.'},
+    {id: 'stink', fed: 'Keep calm and carry on. You can do it if you are calm and collected.'}]
 }
 
 module.exports = levelTwo
--- routes.js
+++ routes.js
@@ -17,8 +17,23 @@ router.get('/:level1', (req, res) => {
 })
 
 router.get('/:level1/:level2', (req, res) => {
+  const l1 = req.params.level1
   const l2 = req.params.level2
-  res.render(l2)
+
+  const data = {
+    levelTwo: levelTwo[l1]
+  }
+
+  function find (temp) {
+    for (let i = 0; i < temp.length; i++) {
+      if (temp[i].id === l2) {
+        return temp[i]
+      }
+    }
+  }
+
+  const result = find(data.levelTwo)
+  res.render('feedback', result)
 })
 
 module.exports = router

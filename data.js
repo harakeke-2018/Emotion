7572374d22f81e8797da4fd417d4bf292bfda74a
Julia Niall <jnmareike@gmail.com>
2018-01-19 14:18:46 +1300

    app working

--- data.js
+++ data.js
@@ -2,8 +2,8 @@
 // const level2sad = [{id: 'gutted'}, {id: 'mellow'}, {id: 'bummer'}, {id: 'stink'}]
 
 const levelTwo = {
-  happy: [{id: 'stoked'}, {id: 'frothed'}, {id: 'joyful'}, {id: 'peaceful'}],
-  sad: [{id: 'gutted'}, {id: 'mellow'}, {id: 'bummer'}, {id: 'stink'}]
+  happy: [{id: 'stoked'}, {id: 'frothing'}, {id: 'joyful'}, {id: 'peaceful'}],
+  sad: [{id: 'gutted'}, {id: 'mellow'}, {id: 'bummed'}, {id: 'stink'}]
 }
 
 module.exports = levelTwo
